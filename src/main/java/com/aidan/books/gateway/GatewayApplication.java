81703f36ebf935b1cae36a1bbe41daf327c6b305
Aidan Whiteley <aidantemp-git@yahoo.co.uk>
2019-12-06 22:05:14 +0000

    Starting to add config properties

--- src/main/java/com/aidan/books/gateway/GatewayApplication.java
+++ src/main/java/com/aidan/books/gateway/GatewayApplication.java
@@ -2,10 +2,12 @@ package com.aidan.books.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 
+@ConfigurationProperties
 @SpringBootApplication
 public class GatewayApplication {
 
---
+++ src/main/java/com/aidan/books/gateway/config/RoutesConfigProperties.java
@@ -0,0 +1,7 @@
+package com.aidan.books.gateway.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "books.gateway")
+public class RoutesConfigProperties {
+}
